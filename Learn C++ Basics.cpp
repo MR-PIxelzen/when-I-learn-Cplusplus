6f308dc25c37a5285374af2b8157f360cf957b01
PiXZeN <4kgaming077@gmail.com>
2023-12-03 13:10:43 +0530

    moew☺

---
+++ Learn C++ Basics.cpp
@@ -0,0 +1,55 @@
+/*
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
+
+
+#include <iostream>
+
+int main() {
+	// Variable declaration
+	int age = 8;
+	float height = 60.1;
+	char grade = 'w';
+
+	// Output variables
+	std::cout << "ram: " << age << std::endl;
+	std::cout << "fps: " << height << std::endl;
+	std::cout << "key: " << grade << std::endl;
+
+	return 0;
+}
+
+
+#include <iostream>
+
+int main() {
+	// Variable declaration
+	int number;
+
+	// Input from the user
+	std::cout << "Enter a number: ";
+	std::cin >> number;
+
+	// Output the entered number
+	std::cout << "You entered: " << number << std::endl;
+
+	return 0;
+}
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "heLOP!" << endl;
+	return 0;
+
+
+
+}
+
